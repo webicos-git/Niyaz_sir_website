fa3fd3d137b16cfbf898f5c62a8763d87860ed52
Ashraf-Sonde <ashrafsonde@gmail.com>
2023-02-26 16:29:45 +0530

    Update: modified the gallery and navbar

--- js/form.js
+++ js/form.js
@@ -1,17 +1,15 @@
-const form = document.getElementById('contact-form');
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  let name = form.elements['name'].value;
-  let email = form.elements['email'].value;
-  let phone = form.elements['phone'].value;
-  let subject = form.elements['subject'].value;
-  let msg = form.elements['message'].value;
+function submitForm() {
+	let name = $("#name").val();
+	let email = $("#email").val();
+	let phone = $("#phone").val();
+	let subject = $("#subject").val();
+	let msg = $("#message").val();
 
-  const message = `Details and query are- Name: ${name}, Email: ${email}, Phone: ${phone}, Subject: ${subject}, Message: ${msg}`;
-  const whatsappURL = `https://api.whatsapp.com/send?phone=919224209876&text=${message}`;
-  let a = document.createElement('a');
-  a.href = whatsappURL;
-  a.target = '_blank';
-  a.click();
-  window.location.replace('/');
-});
+	const message = `Details and query are- Name: ${name}, Email: ${email}, Phone: ${phone}, Subject: ${subject}, Message: ${msg}`;
+	const whatsappURL = `https://api.whatsapp.com/send?phone=919224209876&text=${message}`;
+	let a = document.createElement("a");
+	a.href = whatsappURL;
+	a.target = "_blank";
+	a.click();
+	// window.location.replace('/');
+}
